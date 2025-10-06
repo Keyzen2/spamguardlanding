ea6f482784c473416b0af65f5fac3a6494282b2a
Keyzen2 <keyzen@protonmail.com>
2025-10-06 18:13:15 +0200

    Update Download.tsx

--- components/Download.tsx
+++ components/Download.tsx
@@ -13,46 +13,25 @@ export default function Download() {
         
         <div className="mt-10 flex flex-col sm:flex-row justify-center gap-4">
           
-            href="https://github.com/tu-usuario/spamguard-wordpress/releases/latest/download/spamguard.zip"
+            href="https://github.com/Keyzen2/spamguard/releases/latest"
             className="inline-flex items-center justify-center rounded-full bg-white px-8 py-4 text-lg font-semibold text-indigo-600 shadow-lg hover:bg-gray-100 transition-all duration-200"
           >
-            <svg className="mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
             Descargar Plugin
           </a>
           
-            href="https://github.com/tu-usuario/spamguard"
+            href="https://github.com/Keyzen2/spamguard"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center justify-center rounded-full border-2 border-white px-8 py-4 text-lg font-semibold text-white hover:bg-white/10 transition-all duration-200"
           >
-            <svg className="mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-              <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-            </svg>
             Ver en GitHub
           </a>
         </div>
         
         <div className="mt-8 flex items-center justify-center space-x-6 text-sm text-indigo-200">
-          <div className="flex items-center">
-            <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-            WordPress 5.8+
-          </div>
-          <div className="flex items-center">
-            <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-            PHP 7.4+
-          </div>
-          <div className="flex items-center">
-            <svg className="mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-            Instalacion en 1 minuto
-          </div>
+          <div className="flex items-center">WordPress 5.8+</div>
+          <div className="flex items-center">PHP 7.4+</div>
+          <div className="flex items-center">Instalacion en 1 minuto</div>
         </div>
       </div>
     </section>
