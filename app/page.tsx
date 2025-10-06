d03eb7d2178305700bd2a9af3ba39ff605d2cff5
Keyzen2 <keyzen@protonmail.com>
2025-10-06 16:13:39 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import Stats from '@/components/Stats'
+import FAQ from '@/components/FAQ'
+import Download from '@/components/Download'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-slate-50 to-white">
+      <Hero />
+      <Stats />
+      <Features />
+      <Download />
+      <FAQ />
+    </main>
+  )
+}
